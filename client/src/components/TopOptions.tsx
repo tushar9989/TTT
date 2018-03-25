5bfbee7bc5762646640093c63b19123a3f4c8ca3
tushar.d <tushar.d@media.net>
2018-03-26 03:50:17 +0530

    All functionality now working. Styling remains.

--- client/src/components/TopOptions.tsx
+++ client/src/components/TopOptions.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Form, FormGroup, ControlLabel, FormControl, Button, HelpBlock, InputGroup, Checkbox } from "react-bootstrap";
+import { Redirect } from "react-router-dom";
 
 export interface TopProps {
     defaultUrl?: string,
@@ -11,7 +12,8 @@ export interface TopProps {
 export interface TopState {
     url: string,
     count: number,
-    ignoreCommon: boolean
+    ignoreCommon: boolean,
+    redirect: boolean
 }
 
 export class TopOptions extends React.Component<TopProps, TopState> {
@@ -27,10 +29,13 @@ export class TopOptions extends React.Component<TopProps, TopState> {
         this.state = {
             url: props.defaultUrl,
             count: props.minCount,
-            ignoreCommon: props.ignoreCommon
+            ignoreCommon: props.ignoreCommon,
+            redirect: false
         }
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handleUrlChange = this.handleUrlChange.bind(this);
+        this.handleCountChange = this.handleCountChange.bind(this);
+        this.handleCheckedChange = this.handleCheckedChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
     
@@ -48,15 +53,42 @@ export class TopOptions extends React.Component<TopProps, TopState> {
         return null;
     }
 
-    handleChange(e: any) {
+    handleUrlChange(e: any) {
         this.setState({ url: e.target.value } as TopState);
     }
 
+    handleCountChange(e: any) {
+        this.setState({ count: e.target.value });
+    }
+
+    handleCheckedChange(e: any) {
+        this.setState({ ignoreCommon: e.target.checked });
+    }
+
     handleSubmit() {
-        console.log(this.state);
+        this.setState({
+            redirect: true
+        });
     }
 
     render() {
+        if(this.state.redirect) {
+            var to = '/top/' + this.state.count;
+            var search = [];
+
+            if(this.state.url && this.state.url !== '') {
+                search.push('url=' + encodeURIComponent(this.state.url));
+            }
+
+            if(this.state.ignoreCommon) {
+                search.push('ignoreCommon=' + this.state.ignoreCommon)
+            }
+
+            return <Redirect push to = {{
+                pathname: to,
+                search: '?' + search.join('&')
+            }} />;
+        }
     return (
     <div>
             <form>
@@ -71,7 +103,7 @@ export class TopOptions extends React.Component<TopProps, TopState> {
                             type="text"
                             value={this.state.url}
                             placeholder={this.props.defaultUrl}
-                            onChange={this.handleChange}
+                            onChange={this.handleUrlChange}
                         />
                     </InputGroup>
                     <FormControl.Feedback />
@@ -86,11 +118,14 @@ export class TopOptions extends React.Component<TopProps, TopState> {
                         min={this.props.minCount}
                         type="number"
                         defaultValue={String(this.props.minCount)}
+                        onChange={this.handleCountChange}
                         />
                     </InputGroup>
                 </FormGroup>
                 <FormGroup>
-                    <Checkbox defaultChecked={this.state.ignoreCommon}>Ignore Common Words</Checkbox>
+                    <Checkbox 
+                    defaultChecked={this.state.ignoreCommon}
+                    onChange={this.handleCheckedChange}>Ignore Common Words</Checkbox>
                 </FormGroup>
                 <Button onClick={this.handleSubmit}>GO</Button>
             </form>
--- client/src/components/TopTable.tsx
+++ client/src/components/TopTable.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
+import { Table, ProgressBar, Pagination } from 'react-bootstrap';
 import { RouteComponentProps } from 'react-router-dom';
+import * as $ from 'jquery';
 
 // Define what your match should look like
 interface TopTableMatch {
@@ -11,10 +13,220 @@ interface TopTableParentProps { }
 type TopTableProps = TopTableParentProps &
     RouteComponentProps<TopTableMatch>;
 
-export class TopTable extends React.Component<TopTableProps> {
+export interface TopTableState {
+    url: string,
+    count: number,
+    ignoreCommon: boolean,
+    data: Array<any>,
+    loading: boolean,
+    error?: string,
+    pageNo: number,
+    pageSize: number
+}
+
+export class TopTable extends React.Component<TopTableProps, TopTableState> {
+
+    constructor(props: TopTableProps) {
+        super(props);
+
+        let parsedParams: any = {};
+        var queryParams = props.location.search.substring(1).split('&');
+        for(var i = 0; i < queryParams.length; i++)
+        {
+            var keyAndValue = queryParams[i].split('=');
+            parsedParams[keyAndValue[0]] = decodeURIComponent(keyAndValue[1]);
+        }
+
+        if (this.props.match.params.count < 1)
+        {
+            this.props.match.params.count = 1;
+        }
+
+        this.state = {
+            url: parsedParams.url === undefined ? 'http://terriblytinytales.com/test.txt' : parsedParams.url,
+            count: this.props.match.params.count,
+            ignoreCommon: parsedParams.ignoreCommon === undefined ? true : parsedParams.ignoreCommon,
+            data: [],
+            loading: true,
+            pageNo: 1,
+            pageSize: 20
+        };
+
+        this.handlePageClick = this.handlePageClick.bind(this);
+    }
+
+    componentDidMount() {
+        var endpoint = '/api/top/' + this.state.count;
+        $.ajax({
+            url: endpoint,
+            dataType: 'json',
+            data: {
+                url: this.state.url,
+                ignoreCommon: this.state.ignoreCommon
+            },
+            success: (response: any) => {
+                this.setState({
+                    data: response.data,
+                    loading: false
+                });
+            },
+            error: (data: any) => {
+                this.setState({
+                    error: data.responseJSON.message,
+                    loading: false
+                })
+            }
+        });
+    }
+
     render() {
+
+        if(this.state.loading) {
+            return <ProgressBar active now={100} />;
+        }
+
+        if(this.state.error) {
+            return <div>{this.state.error}</div>
+        }
+
+        var table = this.getTable(this.state);
+
+        var pagination = this.getPagination(this.state.pageNo, this.state.pageSize, this.state.data.length);
+
         return (
-            <div>{this.props.match.params.count}</div>
+            <div>
+                {table}
+                {pagination}
+            </div>
+        );
+    }
+
+    handlePageClick(e: any) {
+        let targetPage = e.target.innerHTML;
+
+        if (targetPage === '‹')
+        {
+            targetPage = this.state.pageNo - 1;
+        }
+        else if (targetPage === '›')
+        {
+            targetPage = this.state.pageNo + 1;
+        }
+        else
+        {
+            targetPage = parseInt(targetPage);
+        }
+
+        this.setState({
+            pageNo: targetPage
+        });
+    }
+
+    getPagination(pageNo: number, pageSize: number, max: number) {
+        let items = [];
+        let lastIndex = Math.floor(max / pageSize);
+
+        if(pageNo !== 1)
+        {
+            items.push(<Pagination.Prev onClick={this.handlePageClick} key="prev" />);
+        }
+
+        if(lastIndex <= 10)
+        {
+            for (let number = 1; number <= lastIndex; number++) {
+                let item = <Pagination.Item onClick={this.handlePageClick} key={number} active={number === pageNo}>{number}</Pagination.Item>;
+                items.push(item);
+            }
+        }
+        else
+        {
+            let left = pageNo - 3;
+            let right = pageNo + 3;
+            let addRight = false;
+
+            if(left < 1)
+            {
+                while (left < 1) {
+                    left++;
+                    right++;
+                }
+            }
+            else if(left !== 1)
+            {
+                items.push(<Pagination.Item onClick={this.handlePageClick} key="1">{1}</Pagination.Item>);
+                items.push(<Pagination.Ellipsis key="ell-prev" />);
+            }
+
+            if(right > lastIndex)
+            {
+                while (right > lastIndex) {
+                    left--;
+                    right--;
+                }
+            }
+            else if(right !== lastIndex)
+            {
+                addRight = true;
+            }
+
+            for (let number = left; number <= right; number++) {
+                let item = <Pagination.Item onClick={this.handlePageClick} key={number} active={number === pageNo}>{number}</Pagination.Item>;
+                items.push(item);
+            }
+
+            if(addRight)
+            {
+                items.push(<Pagination.Ellipsis key="ell-next" />);
+                items.push(<Pagination.Item onClick={this.handlePageClick} key={lastIndex}>{lastIndex}</Pagination.Item>);
+            }
+        }
+        
+        if(pageNo !== lastIndex)
+        {
+            items.push(<Pagination.Next onClick={this.handlePageClick} key="next" />);
+        }
+
+        return (
+            <div>
+                <Pagination bsSize="small">{items}</Pagination>
+            </div>
+        );
+    }
+
+    getTable(state: TopTableState) {
+        var data;
+        if (state.data) {
+            data = state.data;
+
+            var start = (state.pageNo - 1) * state.pageSize;
+
+            data = data.slice(start, start + state.pageSize);
+        }
+        else {
+            data = [];
+        }
+
+        return (
+            <div>
+                <Table bordered condensed hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Word</th>
+                            <th>Count</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((row, i) =>
+                            <tr key={i}>
+                                <td key="1">{((state.pageNo - 1) * state.pageSize) + i + 1}</td>
+                                <td key="2">{row.word}</td>
+                                <td key="3">{row.count}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </Table>
+            </div>
         );
     }
 }
